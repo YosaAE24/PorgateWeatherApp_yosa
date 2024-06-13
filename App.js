90c4e04df275e3a822802c0b0ac688bd3ef85195
YosaAE24 <cebastin2424@gmail.com>
2024-06-13 23:22:41 +0700

    add loading and error screens to weather app

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import {View, StyleSheet, ActivityIndicator, Text} from 'react-native'
+import WeatherSearch from './src/components/weatherSearch'
+import WeatherInfo from './src/components/weatherInfo'
+import axios from 'axios'
+import {BASE_URL, API_KEY} from './src/components/constan'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+	const [weatherData, setWeatherData] = useState()
+	const [status, setStatus] = useState('')
+	const searchWeather = (location) => {
+		setStatus('loading')
+		axios
+			.get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+			.then((response) => {
+				const data = response.data
+				data.visibility /= 1000
+				data.visibility = data.visibility.toFixed(2)
+				data.main.temp -= 273.15
+				data.main.temp = data.main.temp.toFixed(2)
+				setWeatherData(data)
+				setStatus('success')
+			})
+			.catch((error) => {
+				setStatus('error')
+			})
+	}
+
+	const renderComponent = () => {
+		switch (status) {
+			case 'loading':
+				return <ActivityIndicator size={'large'} />
+			case 'success':
+				return <WeatherInfo weatherData={weatherData} />
+			case 'error':
+				return (
+					<Text>
+						Something went wrong. Please try again with a correct
+						city name.
+					</Text>
+				)
+			default:
+				return
+		}
+	}
+
+	return (
+		<View style={styles.container}>
+			<WeatherSearch searchWeather={searchWeather} />
+			<View style={styles.marginTop20}>{renderComponent()}</View>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		padding: 20,
+	},
+	marginTop20: {
+		marginTop: 20,
+	},
+})
+
+export default App
